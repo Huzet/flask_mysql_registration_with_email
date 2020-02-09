5822a1a0b70d38bf20dbc87eae89dfb58830f3db
Tomas <tomas.huzevka@outlook.com>
2020-02-09 14:35:15 -0500

    fixed email syntax issue

--- server.py
+++ server.py
@@ -15,28 +15,36 @@ def index():
 
 @app.route('/add', methods=['POST'])
 def add():
-
+    is_valid = True
     if len(request.form['FN']) < 2:
-    	flash("Please enter a first name")
+        is_valid = False
+        flash("Please enter a first name")
     if len(request.form['LN']) < 2:
-    	flash("Please enter a last name")
+        is_valid = False
+        flash("Please enter a last name")
     if len(request.form['EM']) < 2:
-    	flash("Occupation should be at least 2 characters")    
+        is_valid = False
+        flash("Occupation should be at least 2 characters")    
     if len(request.form['PW']) < 2:
-    	flash("Occupation should be at least 2 characters")     
-    if not '_flashes' in session.keys():
-        flash("Friend successfully added!") 
-
-    query = 'INSERT INTO registrations (first_name, last_name, email, password, updated_at, created_at) VALUES (%(FN)s, %(LN)s, %(EM)s, %(PW)s, NOW(), NOW())'
-    data = {
-        'FN' : request.form['FN'], 
-        'LN' : request.form['LN'],
-        'EM' : request.form['EM'],
-        'PW' : request.form['PW'],
-    }
-    print(data)
-    test = connectToMySQL('registration_with_email')
-    test.query_db(query, data)
+        is_valid = False
+        flash("Occupation should be at least 2 characters")      
+    if not EMAIL_REGEX.match(request.form['EM']):
+        is_valid = False
+        flash("Invalid email address!")
+    
+    if is_valid:
+        query = 'INSERT INTO registrations (first_name, last_name, email, password, updated_at, created_at) VALUES (%(FN)s, %(LN)s, %(EM)s, %(PW)s, NOW(), NOW())'
+        data = {
+            'FN' : request.form['FN'], 
+            'LN' : request.form['LN'],
+            'EM' : request.form['EM'],
+            'PW' : request.form['PW'],
+        }
+        print(data)
+        mysql = connectToMySQL('registration_with_email')
+        mysql.query_db(query, data)
+        flash("Added friend")
+    
     return redirect('/')
 
 if __name__ == '__main__':
